c1e82bbc5b842a6cb664d35cc56582ac656fb119
Daniel Bulger <dpbulger@gmail.com>
2012-01-06 23:25:02 -0500

    Tweaked piece placement variables
    Still not perfect...
    
    Signed-off-by: Daniel Bulger <dpbulger@gmail.com>

--- Piece.cpp
+++ Piece.cpp
@@ -18,11 +18,11 @@ Piece::Piece(int player, Board *parent):parent(parent),player(player)
 float Piece::OFFSET_LEFT(){
     return (-1.5/1574.0)*(parent->width());
 }
-float Piece::COL_WIDTH(){return (169.0/1574.0)*(parent->originalWidth);}
+float Piece::COL_WIDTH(){return (169.0/1574.0)*float(parent->originalWidth);}
 float Piece::COL_HEIGHT(){return COL_WIDTH();}
-float Piece::H_SPACING(){return (41.9/1574)*(parent->originalWidth);}
-float Piece::V_SPACING(){return (21.5/1260.0)*(parent->originalHeight);}
-float Piece::OFFSET_BOTTOM(){return (68.0/1260)*(parent->originalHeight);}
+float Piece::H_SPACING(){return (40.6/1574.0)*float(parent->originalWidth);}
+float Piece::V_SPACING(){return (21.0/1260.0)*float(parent->originalHeight);}
+float Piece::OFFSET_BOTTOM(){return (68.0/1260.0)*float(parent->originalHeight);}
 void Piece::moveToCol(int col)
 {
     this->setX(OFFSET_LEFT()+(COL_WIDTH()+H_SPACING())*(col-1));
